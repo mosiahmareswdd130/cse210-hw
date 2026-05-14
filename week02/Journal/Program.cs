d215134973bb97e457dd30fb54748e8b6a169bae
mosiahmareswdd130 <jmosiahmaresg@gmail.com>
2026-05-13 23:11:59 -0600

    #commit

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        // First job
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Second job
+        Job job2 = new Job();
+
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Create resume
+        Resume myResume = new Resume();
+
+        myResume._name = "John Doe";
+
+        // Add jobs to the list
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display the resume
+        myResume.Display();
     }
 }
---
+++ week02/Journal/Resume.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+public class Resume
+{
+    
+    public string _name;
+
+    
+    public List<Job> _jobs = new List<Job>();
+
+    
+    public void Display()
+    {
+        // Display person's name
+        Console.WriteLine(_name);
+
+        
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
