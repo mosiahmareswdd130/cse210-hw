315cc1adbc3eb61315b901b9ee04b7779efa0f1b
mosiahmareswdd130 <jmosiahmaresg@gmail.com>
2026-06-04 22:09:44 -0600

    #commit

---
+++ week05/Mindfulness/BreathingActivity.cs
@@ -0,0 +1,31 @@
+namespace Mindfulness;
+
+public class BreathingActivity : Activity
+{
+    public BreathingActivity()
+        : base("Breathing Activity",
+    "This activity will help you relax by walking you through breathing in and out slowly. " +
+               "Clear your mind and focus on your breathing.")
+    {
+    }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+
+        DateTime end = DateTime.Now.AddSeconds(_durationSeconds);
+        bool breatheIn = true;
+
+        while (DateTime.Now < end)
+        {
+            if (breatheIn)
+                Console.WriteLine("Breathe in...");
+            else
+                Console.WriteLine("Breathe out...");
+
+            ShowCountdown(4);
+            breatheIn = !breatheIn;
+        }
+        DisplayMessage();
+    }
+}
