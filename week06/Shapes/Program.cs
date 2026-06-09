c1bf78af50278bae4b59f839efc5a00b95482515
mosiahmareswdd130 <jmosiahmaresg@gmail.com>
2026-06-08 20:09:28 -0600

    #commit

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
+using Shapes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+
+        Square s2 = new Square("Blue", 5);
+        shapes.Add(s2);
+
+        Square s3 = new Square("Yellow", 8);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+        Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
